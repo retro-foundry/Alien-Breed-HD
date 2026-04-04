31a86cf3cfa0be4ace1977ea795c92f8231499a3
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-04-04 15:57:04 +0100

    Disable Debug Textures Dump

--- src/control_loop.c
+++ src/control_loop.c
@@ -497,7 +497,6 @@ void play_game(GameState *state)
     io_load_floor();
     io_load_sky();
     io_load_gun_graphics();
-    io_dump_textures();  /* Debug: write textures/*.bmp for viewing */
     io_load_objects();
     io_load_vec_objects();
     io_load_sfx();
